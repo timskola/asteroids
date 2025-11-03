2c3bd8c0bd1844d58c5786e22b4a9f5df9d1ca79
Tim, Nilsson <tim.nilsson@edu.sgy.se>
2025-11-03 15:47:16 +0100

    Update game.py

--- game.py
+++ game.py
@@ -2,25 +2,25 @@ import pygame
 
 class Asteroids:
     def __init__(self):
-        self.__init__pygame()
+        self._init_pygame()
         self.screen = pygame.display.set_mode((800, 600))
 
-        def main_loop(self):
-            while True:
-                self._handle_input()
-                self._process_game_logic()
-                self._draw()
+    def main_loop(self):
+        while True:
+            self._handle_input()
+            self._process_game_logic()
+            self._draw()
+            
+    def _init_pygame(self):
+        pygame.init()
+        pygame.display.set_caption("Asteroids but awesome")
 
-        def _init_pygame(self):
-            pygame.init()
-            pygame.display.set_caption("Asteroids but awesome")
+    def _handle_input(self):
+        pass
 
-        def _handle_input(self):
-            pass
+    def _process_game_logic(self):
+        pass
 
-        def _process_game_logic(self):
-            pass
-
-        def _draw(self):
-            self.screen.fill((0, 0, 0))
-            pygame.display.flip()
+    def _draw(self):
+        self.screen.fill((0, 0, 0))
+        pygame.display.flip()
